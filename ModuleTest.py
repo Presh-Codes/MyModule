50829648d45b0f9ce18b84f69b2af916c99d0a20
Presh-Codes <110492316+Presh-Codes@users.noreply.github.com>
2023-01-01 14:48:08 +0000

    Add files via upload

---
+++ ModuleTest.py
@@ -0,0 +1,5 @@
+import damn as dm
+
+dm.shit("Precious", 13, "10th")
+
+print("My favourite fruits are", dm.fruits[6] + ",", dm.fruits[9], "and", dm.fruits[10])
---
+++ damn.py
@@ -0,0 +1,7 @@
+def shit(name, age, grade):
+    print("Hello there, my name is", name, "and I am", age, "years old")
+    print("I am in the", grade, "grade")
+
+
+fruits = ["apple", "banana", "mango", "watermelon", "pear", "grape", "orange", "guava", "pineapple", "cherry",
+          "blue berry"]
